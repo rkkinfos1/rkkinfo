6b7705944001041491145a31b7a6716b56711898
dtutrainee <shubhamsharma160792@gmail.com>
2013-02-11 19:20:28 +0530

    plan added

--- app/models/oneyear_customer.rb
+++ app/models/oneyear_customer.rb
@@ -4,7 +4,7 @@ class OneyearCustomer < ActiveRecord::Base
 	 scope :new_customer,where( 'created_at between ? and ?', Date.today, Date.tomorrow)
     validates :e_mail, :format => { :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/,
     :message => "Wrong Format" }
-     validates_presence_of :emp_no, :e_mail, :name, :phone, :plan
+     validates_presence_of :emp_no, :e_mail, :name, :phone
      def self.search(search)
   if search
     order('created_at desc').find(:all, :conditions => ['cast(emp_no as text) LIKE ?', "%#{search}%"])
