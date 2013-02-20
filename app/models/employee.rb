5d926c607f3e04301c41e3ed2bab2d771cba4750
dtutrainee <shubhamsharma160792@gmail.com>
2013-02-20 19:01:57 +0530

    validation on cv

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,7 @@
 class Employee < ActiveRecord::Base
   attr_accessible :name, :no, :address, :tel, :email, :salary, :date_of_joining, :date_of_resignation, :cv
   validates :no, :uniqueness => true
-  validates_presence_of :name, :no, :address, :tel, :email, :salary, :date_of_joining, :cv
+  validates_presence_of :name, :no, :address, :tel, :email, :salary, :date_of_joining
   has_attached_file :cv,
                     :url  => "/assets/products/:id/original/:basename.:extension",
                     :path => ":rails_root/public/assets/products/:id/original/:basename.:extension"
